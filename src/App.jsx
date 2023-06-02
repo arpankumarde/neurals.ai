b8bf9d5013453285d416f1361e4ef948b94461a5
arpankumarde <academiaindiana@outlook.com>
2023-06-02 19:46:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { places } from './api/places';
-import { Searchbar, Mapspace, Sidebar } from './components';
 
 function App() {
   const [map, setMap] = useState("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21167573.757009193!2d78.03745447728565!3d23.66765408621442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sus!4v1685703314199!5m2!1sen!2sus")
