7a05965ecbbddb2f9936f6f19b34e6d37b0b2ae9
arpankumarde <academiaindiana@outlook.com>
2023-06-02 22:31:33 +0530

    updates formatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { places } from './api/places';
 
 function App() {
-  const [map, setMap] = useState("https://www.google.com/maps?q=India,India&output=embed");
+  const [map, setMap] = useState("https://www.google.com/maps?q=India&output=embed");
   const [majCity, setMajCity] = useState('');
 
   function changeMapSrc(code) {
@@ -58,22 +58,21 @@ function App() {
   }
 
   return (
-    <div className="flex w-full min-h-screen">
+    <div className="flex w-full min-h-screen select-none">
 
-      {/** LOADING SPINNER */}
-      <div className="loader-container">
-        <div className="spinner"></div>
-      </div>
       {/** SEARCHBAR */}
       <aside className='w-[25%] m-2 pr-2 flex flex-col justify-between border-r-[1px] border-zinc-200'>
         <div className="placeFrame w-full">
-          <div id="searchbar" className="mb-2">
-            <input id="search" type="text" placeholder="Search..." className='border p-3 border-black rounded-full h-10 w-full text-lg' onChange={sortList} />
+          <div id="searchbar" className="mb-2 flex items-center border-sky-500 hover:border-sky-600 border-2 rounded-full px-3 text-lg">
+            <span class="material-icons-outlined text-lg">
+              search
+            </span>
+            <input id="search" type="text" placeholder="Search..." className='outline-none ml-2 rounded-full h-10 w-full text-lg' onChange={sortList} />
           </div>
           <div id='btnList'>
             {bigPlaces.map(item => (
-              <button key={item} onClick={() => setMajorURL(item)} type='button' className='listBtn hover:bg-zinc-200 hover:shadow-lg active:bg-zinc-300 w-full lg:text-left py-3 px-4 rounded-lg my-1 text-lg flex items-center gap-1'>
-                <span className="material-icons-outlined text-base">
+              <button key={item} onClick={() => setMajorURL(item)} type='button' className={`${(item == majCity) ? 'bg-zinc-300 hover:bg-zinc-300 shadow-lg' : ''} listBtn hover:bg-zinc-200 hover:shadow-lg active:bg-zinc-300 active:shadow-lg w-full lg:text-left py-3 px-4 rounded-lg my-1 text-lg flex items-center gap-1`}>
+                <span className="material-icons-outlined text-base text-sky-600">
                   north_east
                 </span>
                 {item}
@@ -87,7 +86,7 @@ function App() {
         </div>
         <div className="text-center mt-2">
           Developed by
-          <a href="https://linkedin.com/in/arpan-kumar-de/" className="text-blue-700 font-semibold">
+          <a href="https://linkedin.com/in/arpan-kumar-de/" className="text-sky-700 font-semibold">
             &nbsp;Arpan
           </a>
         </div>
@@ -107,19 +106,19 @@ function App() {
       </div>
 
       {/** SIDEBAR */}
-      <aside className='h-[97.5vh] w-[25%] m-2 pl-2 border-l-[1px] border-zinc-300 flex flex-col'>
+      <aside className='h-[97.5vh] w-[25%] m-2 pl-2 border-l-[1px] border-zinc-300 flex flex-col gap-2'>
         <div className="flex h-[60%] flex-col overflow-y-scroll" id="minorBtnList">
           {(majCity != '') && (places[majCity].map(item2 => (
             <button key={item2} onClick={() => changeMapSrc(item2)} type='button' className='listBtn hover:bg-zinc-200 hover:shadow-lg active:bg-zinc-300 w-full lg:text-left py-3 px-4 rounded-lg my-1 text-lg flex items-center gap-1'>
-              <span className="material-icons-outlined text-base">
-                north_east
+              <span className="material-icons-outlined text-base text-sky-600">
+                place
               </span>
               {item2}
             </button>)
           ))}
         </div>
-        <div className="flex h-[40%] text-center mt-4 border-t-[1px] border-zinc-200 w-full">
-          <div className="flex justify-center w-full">
+        <div className="flex h-[40%] text-center border-t-[1px] border-zinc-200 w-full">
+          <div className="flex justify-center w-full items-center">
             Instagram Section
           </div>
         </div>
--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Sidebar from "./sidebar";
-import Searchbar from "./searchbar";
-import Mapspace from "./mapspace";
-
-export {
-    Searchbar,
-    Mapspace,
-    Sidebar,
-}
