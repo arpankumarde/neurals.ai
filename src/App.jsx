824866db4b5296a32e484e73992adbad76aff475
arpankumarde <academiaindiana@outlook.com>
2023-06-02 21:03:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { places } from './api/places';
 
 function App() {
-  const [map, setMap] = useState("https://www.google.com/maps?q=India&output=embed")
+  const [map, setMap] = useState("https://www.google.com/maps?q=India&output=embed");
   const [majCity, setMajCity] = useState('');
 
   function changeMapSrc(code) {
@@ -13,18 +13,9 @@ function App() {
   const bigPlaces = Object.keys(places); //List of Major Cities
 
   function setMajorURL(code) {
-    changeMapSrc(code) //setting iframe to new coordinates of 
-    setMajCity(code);
+    changeMapSrc(code) //setting iframe to new coordinates
+    setMajCity(code); //setting Major City to update Minor City List
   }
-
-  //coordinates of Major Cities
-  const bigCoord = [
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d896701.9007529835!2d76.48341326109504!3d28.607521774510452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi%2C%20India!5e0!3m2!1sen!2sus!4v1685613190375!5m2!1sen!2sus",
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241316.67292461742!2d72.7163707397183!3d19.082502007163477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c6306644edc1%3A0x5da4ed8f8d648c69!2sMumbai%2C%20Maharashtra%2C%20India!5e0!3m2!1sen!2sus!4v1685608969567!5m2!1sen!2sus",
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d497698.77492134983!2d77.3012646607384!3d12.954459535201714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1670c9b44e6d%3A0xf8dfc3e8517e4fe0!2sBengaluru%2C%20Karnataka%2C%20India!5e0!3m2!1sen!2sus!4v1685609092820!5m2!1sen!2sus",
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117925.216896607!2d88.26495028971651!3d22.535564937385992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f882db4908f667%3A0x43e330e68f6c2cbc!2sKolkata%2C%20West%20Bengal%2C%20India!5e0!3m2!1sen!2sus!4v1685605447241!5m2!1sen!2sus",
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d497511.23106923234!2d79.87933693948655!3d13.047985941436249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5265ea4f7d3361%3A0x6e61a70b6863d433!2sChennai%2C%20Tamil%20Nadu%2C%20India!5e0!3m2!1sen!2sus!4v1685609040193!5m2!1sen!2sus",
-  ];
 
   function countHiddenNodes(li) {
     let c = 0;
